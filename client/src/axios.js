9a147cae336a21caefedcc9e0327519b720b6348
jasmeendesai <130737953+jasmeendesai@users.noreply.github.com>
2024-05-11 16:56:56 +0530

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const makeRequest = axios.create({
-    baseURL : "http://localhost:5000/api/",
+    baseURL : "https://aware-satisfying-belly.glitch.me/api/",
     withCredentials : true,
 })
